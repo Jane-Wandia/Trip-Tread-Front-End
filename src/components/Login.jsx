d855860ff48896b9cef52f1490fb8c0700ce4132
cynthia <cchelangat9892gmail.com>
2022-12-05 21:09:26 +0300

    Styled the login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import './CSS/Login.css';
+import React from "react";
+import "./CSS/Login.css";
 
 function Login() {
   return (
-    <div className='login-box'>
+    <div className="login-box">
       <h2>Login</h2>
       <form>
-        <div className='user-box'>
-          <input type='text' name='' required=''></input>
+        <div className="user-box">
+          <input type="text" name="" required=""></input>
           <label>Username</label>
-
         </div>
-        <div className='user-box'>
-          <input type='text' name='' required=''></input>
+        <div className="user-box">
+          <input type="text" name="" required=""></input>
           <label>Password</label>
-          
         </div>
+        <button className="submit-btn" type="submit">
+          Submit
+        </button>
       </form>
-      </div>
-  )
+      <a href="#" className="link-btn">Don't have an account? Register here.</a>
+    </div>
+  );
 }
 
 export default Login;
