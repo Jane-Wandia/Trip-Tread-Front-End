9b6a78e528834c1bd056eab04255d06b52fe2fd0
cynthia <cchelangat9892gmail.com>
2022-12-08 16:15:18 +0300

    Created the navlinks

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,36 @@
-import React from 'react';
-import {
-  Nav,
-  NavLink,
-  NavMenu,
-} from './NavBarElements';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import './CSS/NavBar.css'
+import "./CSS/NavBar.css";
 // import logo from '../components/images/logo.jpg'
-  
+
 const Navbar = () => {
   return (
-    <Nav className='navbar'>
-        <NavMenu className='navList'>
-         <h3 className='logo'>Trip Tread </h3>
-          <div className='nav_links'>
-            <NavLink to='/'>
-              Home
-            </NavLink>
-            <NavLink to='/components/Login'>
-              Log Out
-            </NavLink>
-            <NavLink to='/components/About'>
-              About Us
-            </NavLink>
-          </div>
-        </NavMenu>
-    </Nav>
-  );
+    <>
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <h2>
+          Trip<span>Tread</span>
+        </h2>
+      </div>
+
+      <div className="navbar-nav">
+        <ul id="my-nav-links" className="nav-links">
+          <li>
+            <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/logOut">LogOut</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+        </ul>
+      </div>
+    </nav>
+    </>
+ 
+  )
 };
-  
+
 export default Navbar;
