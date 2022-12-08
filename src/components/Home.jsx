a8fb87f53074055c84312447fc8c03935d2eeb9b
cynthia <cchelangat9892gmail.com>
2022-12-08 15:24:43 +0300

    Completed homepage styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,23 +3,33 @@ import "./CSS/Home.css";
 
 function Home(props) {
   return (
-    <div className="home-container">
-      <div className="heading-container">
+    <div className="hero-container">
+      <div className="hero-image">
+        <img
+          src="https://res.cloudinary.com/dphlf7a8o/image/upload/v1670491272/pexels-asad-photo-maldives-2245277_i0gbs2.jpg"
+          alt="image tag"
+        />
+      </div>
+      <div className="preview"></div>
+      <div className="herosection-content">
         <h2>Want to Fly</h2>
         <p>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ualiquip
+          ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum.
         </p>
-      </div>
-      <div>
-        <button className="link-btn" onClick={() => props.onFormSwitch("Home")}>
-          Go to Airlines
-        </button>
+        <div>
+          <button
+            className="link-btn"
+            onClick={() => props.onFormSwitch("Home")}
+          >
+            Go to Airlines
+          </button>
+        </div>
       </div>
     </div>
   );
