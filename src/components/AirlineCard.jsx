a4b797747cbd15bdacf1e13f4bc444ab503004a5
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-12-09 13:37:36 +0300

    added untracked file

---
+++ src/components/AirlineCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./CSS/Airline.css";
+
+function AirlineCard ({airline}) {
+    const airline_id = airline.id
+
+    return (
+        <div class="card">
+            <img id="my-img-uniq" src={airline.image} alt="image not found" />
+            <h3>{airline.name}</h3>
+            <h4 class="iew">Reviews</h4>
+            <Link to="/components/Reviews" state={{ airline_id: airline_id }} class="btn" >Reviews</Link>
+        </div>
+    )
+}
+
+export default AirlineCard
