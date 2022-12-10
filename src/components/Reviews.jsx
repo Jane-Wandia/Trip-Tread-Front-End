ce3725531aaeadd269009832b0e4a399f91b47ab
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-12-10 03:25:55 +0300

    tested

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -13,6 +13,13 @@ function Reviews({ user }) {
   const [airline, setAirline] = useState({});
   const location = useLocation();
   const { airline_id } = location.state || {};
+   const [form, setForm ] = useState({
+    trip:"",
+    review:"",
+    airline_id:airline_id,
+    user_id:user.id
+
+  })
 
   function handleUpdate(e) {
     e.preventDefault();
@@ -31,11 +38,14 @@ function Reviews({ user }) {
         setAirline(data);
         if (data.reviews !== undefined) {
           setReviews(data.reviews);
+          e.target.parentElement.classList.add("show_form")
         } else {
           setReviews([]);
         }
       });
   }
+
+
 
   function handleDelete(id) {
     console.log(id);
@@ -78,7 +88,37 @@ function Reviews({ user }) {
         }
       });
   }, []);
+
+  function handleSubmit(e) {
+  e.preventDefault()
+  fetch("/reviews", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(form ),
+  }).then((r) => {
+    
+    
+    if (r.ok) {
+      
+      r.json().then((form) => setReviews([...reviews, form]));
+      
+      
+    } else {
+      r.json().then((err) => alert(err.error));
+      
+      
+    }
+  });
+  
+}
+const handleVisibility = (e) => {
+  console.log(e.target.parentElement.childNodes[7].classList.remove('show_form'));
+}
+
   return (
+   
     <div className="Container">
 
       <div className="reviews_container">
@@ -89,8 +129,8 @@ function Reviews({ user }) {
             {user.fullname === review.user.fullname ? (
               <>
                 <div className="secondary-container" key={review.id}>
-                  <h3>{review.user.fullname}</h3>
-                  <h3>{review.user.fullname}</h3>
+                  <h3>{`${review.user.fullname}(you)`}</h3>
+                  
                   <h4>{review.trip}</h4>
                   <h5>{airline.name}</h5>
                   <p>{review.review}</p>
@@ -99,7 +139,8 @@ function Reviews({ user }) {
                     id={review.id}
                     onClick={(e) => {
                       setEditId(e.target.id);
-                      setEdit(!edit);
+                      // setEdit(!edit);
+                      handleVisibility(e)
                     }}
                   >
                     Edit
@@ -113,8 +154,22 @@ function Reviews({ user }) {
                   >
                     Delete
                   </button>
+                  <form  id={`form${review.id}`} className="form1 show_form">
+                    <input
+                      placeholder="edit"
+                      name="review"
+                      className="input1"
+                      onChange={(e) =>
+                        setPatch({ ...patch, review: e.target.value })
+                      }
+                      value={patch.review}
+                    />
+                    <button id="button-11" onClick={handleUpdate}>
+                      Submit
+                    </button>
+                  </form>
                 </div>
-                {edit && (
+                {/* {edit && (
                   <form className="form1">
                     <input
                       placeholder="edit"
@@ -129,7 +184,7 @@ function Reviews({ user }) {
                       Submit
                     </button>
                   </form>
-                )}
+                )} */}
               </>
             ) : (
               <>
@@ -149,12 +204,12 @@ function Reviews({ user }) {
         <form>
           <h1>Got Feedback?</h1>
           <label for="trip">your trip:</label>
-          <input type="text" placeholder="Enter your trip.."></input>
+          <input type="text" placeholder="Enter your trip.." onChange={e => setForm({...form, trip:e.target.value})}></input>
 
           <label for="comment">comment:</label>
-          <textarea placeholder="Leave a comment here..."></textarea>
+          <textarea placeholder="Leave a comment here..." onChange={e => setForm({...form, review:e.target.value})}></textarea>
 
-          <button>Submit</button>
+          <button onClick={handleSubmit}>Submit</button>
         </form>
       </div>
     </div>
